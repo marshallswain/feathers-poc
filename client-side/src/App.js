ee2b26397d7ed5865e61ca6a473f1ce7341d48a2
Marshall Thompson <marshall@creativeideal.net>
2017-02-03 11:50:45 -0700

    Fix login on client.

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -3,21 +3,26 @@ import { Header, Container, Segment, Card, Divider } from 'semantic-ui-react';
 import client from './client';
 
 class App extends Component {
-  constructor() {
-    super()
-    client.authenticate({
-      type: 'token',
-      endpoint: '/authentication'
-    })
+  constructor () {
+    super();
+    var loginData = {};
+    if (!window.localStorage.getItem('feathers-jwt')) {
+      loginData = {
+        strategy: 'local',
+        email: 'marshall@creativeideal.net',
+        password: 'feathers'
+      };
+    }
+    client.authenticate(loginData)
     .then(console.log)
-    .catch(console.error)
+    .catch(console.error);
   }
-  render() {
+  render () {
     return (
       <Container>
         <Segment padded>
           <Header dividing size='large'>Feathers Client</Header>
-          <Divider hidden/>
+          <Divider hidden />
           {/* <Card.Group items={this.state.films}/> */}
         </Segment>
       </Container>
